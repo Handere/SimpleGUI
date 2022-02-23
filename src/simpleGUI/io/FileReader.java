9d21e05184787302d5842ee205e32dddebb59c2f
Håkon Skogen <84183241+Handere@users.noreply.github.com>
2022-02-23 16:50:27 +0100

    Add empty methods to FileReader.java, and add documentation to them

--- src/simpleGUI/io/FileReader.java
+++ src/simpleGUI/io/FileReader.java
@@ -1,4 +1,51 @@
 package simpleGUI.io;
 
+import java.util.HashMap;
+
 public class FileReader {
+
+
+    public HashMap<String, Object> readJsonFile(String filePath) {
+        /*
+         * Read the content of the specified json file, and return it as an HashMap<String, Object>.
+         *
+         */
+        return new HashMap<>();
+    }
+
+    public HashMap<String, Object> readCSVFile(String filePath){
+        /*
+         * Read the content of the specified CSV file, and return it as an HashMap<String, Object>.
+         *
+         */
+        return new HashMap<>();
+    }
+
+    public void writeJsonFile(String filePath, Object object){
+        /*
+         * Write the content of the object to the specified json file, by appending it to the end of the file.
+         *
+         */
+    }
+    public void writeJsonFile(String filePath, Object object, boolean append){
+        /*
+         * Write the content of the object to the specified json file.
+         * If append is set to true; the content will be appended to the end of the file.
+         * If append is set to false; the file will be overwritten by the content.
+         */
+    }
+
+    public void writeCSVFile(String filepath, Object object){
+        /*
+         * Write the content of the object to the specified CSV file, by appending it to the end of the file.
+         *
+        */
+    }
+    public void writeCSVFile(String filepath, Object object, boolean append){
+        /*
+         * Write the content of the object to the specified CSV file.
+         * If append is set to true; the content will be appended to the end of the file
+         * If append is set to false; the file will be overwritten by the content.
+        */
+    }
 }
