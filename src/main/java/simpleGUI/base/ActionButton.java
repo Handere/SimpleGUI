08eac0cf80f883a2bd1b94de6300926a0187649a
Håkon Skogen <84183241+Handere@users.noreply.github.com>
2022-02-28 14:33:00 +0100

    Add the frame owner
    Add constructor and set method to get the functionality
    
    Co-Authored-By: hannajohansen <89612756+hannajohansen@users.noreply.github.com>

--- src/main/java/simpleGUI/base/ActionButton.java
+++ src/main/java/simpleGUI/base/ActionButton.java
@@ -4,4 +4,13 @@ import simpleGUI.scene.controls.Button;
 
 public class ActionButton implements Button {
     String buttonName;
+    Frame owner;
+
+    public ActionButton(Frame owner, String buttonName) {
+        this.buttonName = buttonName;
+    }
+
+    public void setButtonName(String buttonName) {
+        this.buttonName = buttonName;
+    }
 }
