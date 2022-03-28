ef2f63e07f5268446ad188cfcb8e245826c072f9
Håkon Skogen <84183241+Handere@users.noreply.github.com>
2022-03-28 16:01:21 +0200

    Add implementation

--- src/main/java/simpleGUI/ActionButton.java
+++ src/main/java/simpleGUI/ActionButton.java
@@ -2,18 +2,35 @@ package simpleGUI;
 
 import simpleGUI.scene.controls.Button;
 
+import javax.swing.*;
+
 /**
  * A button that can be added to a Frame, with the desired action.
  */
 public class ActionButton implements Button {
     /**
-     * The name of the button.
+     * The name of the button. By default, set to "Click me"
      */
-    String buttonName;
+    String buttonName = "Click me";
     /**
      * The Frame the button is attached to.
      */
     Frame owner;
+    /**
+     * The button.
+     */
+    JButton button;
+
+    /**
+     * Constructor.
+     * Create an ActionButton with default name.
+     * @param owner The Frame the button is attached to.
+     */
+    public ActionButton(Frame owner) {
+        this.owner = owner;
+        this.button = new JButton(buttonName);
+        owner.frame.add(button);
+    }
 
     /**
      * Constructor.
@@ -23,6 +40,8 @@ public class ActionButton implements Button {
     public ActionButton(Frame owner, String buttonName) {
         this.buttonName = buttonName;
         this.owner = owner;
+        this.button = new JButton(buttonName);
+        owner.frame.add(button);
     }
 
     /**
@@ -31,5 +50,6 @@ public class ActionButton implements Button {
      */
     public void setButtonName(String buttonName) {
         this.buttonName = buttonName;
+        button.setName(buttonName);
     }
 }
