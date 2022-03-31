83bda5e7a38fdfe70a8b9ba7dea607830161095b
vik2ria <77890073+vik2ria@users.noreply.github.com>
2022-03-31 18:01:28 +0200

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,14 +12,9 @@ public class Main {
     public static void main(String[] args) {
         Window window = new Window();
         window.addFrame();
-        window.getFrame().addActionButton("hello");
-        window.addNavigationBar();
-        MenuButton menuButton1 = window.navigationBar().addNavigationButton("hello");
-        MenuButton menuButton2 = window.navigationBar().addNavigationButton("what?");
-        MenuButton menuButton3 = window.navigationBar().addNavigationButton("wtf?");
-        window.getFrame().addLabel().setSize(4,4,60,50);
-        MultiLineTextBox multiLineTextBox = window.getFrame().addTextBox();
-        multiLineTextBox.setSize(40, 40, 20, 20);
+
+        MultiLineTextBox multiLineTextBox = window.getFrame().addTextBox("vhsdfhjgsdfjhgsdf", true);
+
 
         //window.addScrollbar1();
         //window.addScrollbar(true, true);
