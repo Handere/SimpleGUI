3cdfc357fc0b268ab5c53259bcba7a879239c788
Håkon Skogen <haakosko@hiof.no>
2022-04-02 17:18:02 +0200

    Remove redundant saving of MenuButtons

--- src/main/java/simpleGUI/Menu.java
+++ src/main/java/simpleGUI/Menu.java
@@ -19,10 +19,6 @@ public class Menu {
      * The attached BreadCrumbs.
      */
     BreadCrumbs breadCrumbs;
-    /**
-     * The attached MenuButtons.
-     */
-    ArrayList<MenuButton> menuButtons;
 
     /**
      * Constructor.
