d353b8fde44d1f9f4b3fd8bef42c7cda7f7cb53a
Håkon Skogen <haakosko@hiof.no>
2022-04-02 08:35:21 +0200

    Code cleanup, remove redundant implementation and update constructor

--- src/main/java/simpleGUI/MultiLineTextBox.java
+++ src/main/java/simpleGUI/MultiLineTextBox.java
@@ -10,21 +10,22 @@ import javax.swing.border.TitledBorder;
 public class MultiLineTextBox {
 
     /**
-     * The content of the MultiLineTextBox. Will be displayed.
+     * The text content of the MultiLineTextBox. Will be displayed.
      */
-    String content = "This is a text box";
+    String defaultText = "This is a text box.";
 
-    String borderTitle = "Border title";
-
-    /**
-     * How many rows the MultiLineTextBox should have.
-     */
-    int rows;
+    String defaultBorderTitle = "Border title";
 
     /**
-     * How many columns the MultiLineTextBox should have.
+     * The default number of rows the MultiLineTextBox have.
      */
-    int columns;
+    int defaultNumberOfRows = 15;
+
+    /**
+     * The default number of columns the MultiLineTextBox should have.
+     */
+    int defaultNumberOfColumns = 60;
+
     /**
      * The Frame the MultiLineTextBox is attached to.
      */
@@ -39,49 +40,43 @@ public class MultiLineTextBox {
     JScrollPane scrollbar;
 
     /**
-     * Variables that holds the MultiLineTextBox location and size
-     */
-    int x, y, width, height;
-
-    /**
      * Constructor.
      * @param owner The Frame the MultiLineTextBox is attached to
      */
     public MultiLineTextBox(Frame owner, boolean addScrollbar) {
         this.owner = owner;
-        this.multiLineTextBox = new JTextArea(16,58);
-        buildMultiLineTextBox(addScrollbar);
+        this.multiLineTextBox = new JTextArea(defaultNumberOfRows,defaultNumberOfColumns);
+        buildMultiLineTextBox(addScrollbar, defaultBorderTitle);
     }
 
     /**
      * The Constructor
      * @param owner The frame the MultiLineTextBox is attached to.
-     * @param content The content in the MultiLineTextBox.
+     * @param text The content in the MultiLineTextBox.
      */
-    public MultiLineTextBox(Frame owner, String content, boolean addScrollbar) {
+    public MultiLineTextBox(Frame owner, String text, boolean addScrollbar) {
         this.owner = owner;
-        this.multiLineTextBox = new JTextArea(16, 58);
-        buildMultiLineTextBox(addScrollbar);
+        this.multiLineTextBox = new JTextArea(text, defaultNumberOfRows, defaultNumberOfColumns);
+        buildMultiLineTextBox(addScrollbar, defaultBorderTitle);
     }
 
     /**
      * The Constructor
      * @param owner The frame the MultiLineTextBox is attached to.
-     * @param content The content in the MultiLineTextBox.
+     * @param text The content in the MultiLineTextBox.
      */
-    public MultiLineTextBox(Frame owner, String content, boolean addScrollbar, String borderTitle) {
+    public MultiLineTextBox(Frame owner, String text, boolean addScrollbar, String borderTitle) {
         this.owner = owner;
-        this.multiLineTextBox = new JTextArea(16, 58);
-        this.borderTitle = borderTitle;
-        buildMultiLineTextBox(addScrollbar);
+        this.multiLineTextBox = new JTextArea(text, defaultNumberOfRows, defaultNumberOfColumns);
+        buildMultiLineTextBox(addScrollbar, borderTitle);
     }
 
     /**
      * Set a new content to the MultiLineTextBox.
-     * @param content The content of the MultiLineTextBox. Will be displayed.
+     * @param text The content of the MultiLineTextBox. Will be displayed.
      */
-    public void setContent(String content) {
-        this.content = content;
+    public void setText(String text) {
+        multiLineTextBox.setText(text);
     }
 
     /**
@@ -92,10 +87,6 @@ public class MultiLineTextBox {
      * @param height The height of the MultiLineTextBox.
      */
     public void setSize(int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
         multiLineTextBox.setBounds(x, y, width, height);
     }
 
@@ -104,10 +95,8 @@ public class MultiLineTextBox {
         scrollbar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
     }
 
-    private void buildMultiLineTextBox(boolean addScrollbar) {
-        multiLineTextBox.setText(content);
-        rows = multiLineTextBox.getRows();
-        columns = multiLineTextBox.getColumns();
+    private void buildMultiLineTextBox(boolean addScrollbar, String borderTitle) {
+        multiLineTextBox.setText(defaultText);
         multiLineTextBoxPanel.setBorder(new TitledBorder(new EtchedBorder(), borderTitle));
         if(addScrollbar) {
             addScrollbar();
