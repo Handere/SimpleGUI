c8752e79ebb4df47571f6c2fe3e0af963eba63c6
Handere <84183241+Handere@users.noreply.github.com>
2022-05-02 14:17:32 +0200

    Update javadoc

--- src/main/java/simpleGUI/SingleLineTextLabel.java
+++ src/main/java/simpleGUI/SingleLineTextLabel.java
@@ -4,28 +4,29 @@ import javax.swing.*;
 
 
 /**
- * Label that can be attached and displayed on a Frame.
+ * A single line text label.
  */
 public class SingleLineTextLabel {
 
     /**
-     * The default text when the user don't enter it when the SingleLineTextLabel is created
+     * The default text of the single line text label.
      */
     private final String DEFAULT_TEXT = "Remember to add text";
 
     /**
-     * The Frame the Label is attached to.
+     * The owner frame of the single line text label.
      */
     private Frame owner;
 
     /**
-     * The SingleLineTextLabel
+     * The single line text label.
      */
     private JLabel singleLineTextLabel;
 
     /**
-     * Constructor.
-     * @param owner The Frame the Label is attached to.
+     * Create a single line text label attached to a chosen frame.
+     * Default text.
+     * @param owner The frame.
      */
     public SingleLineTextLabel(Frame owner) {
         this.singleLineTextLabel = new JLabel(DEFAULT_TEXT);
@@ -34,9 +35,9 @@ public class SingleLineTextLabel {
     }
 
     /**
-     * Constructor.
-     * @param owner The Frame the Label is attached to.
-     * @param singleLineText The text inside the Label. Will be displayed.
+     * Create a single line text label with a chosen text, attached to a chosen frame.
+     * @param owner The frame.
+     * @param singleLineText The single line text label.
      */
     public SingleLineTextLabel(Frame owner, String singleLineText) {
         this.singleLineTextLabel = new JLabel(singleLineText);
@@ -45,8 +46,8 @@ public class SingleLineTextLabel {
     }
 
     /**
-     * Set the title of the Label.
-     * @param singleLineText The singleLineText of the Label. Will be displayed.
+     * Set the title of the single line text label.
+     * @param singleLineText The single line text label.
      */
     public void setTitle(String singleLineText) {
         singleLineTextLabel.setText(singleLineText);
@@ -54,11 +55,11 @@ public class SingleLineTextLabel {
 
 
     /**
-     * Set the size of the label and location
+     * Set the size and location of the single line text label.
      * @param x X-coordinate for top left corner.
      * @param y Y-coordinate for top left corner.
-     * @param width The width of the label.
-     * @param height The height of the label.
+     * @param width The width of the single line text label.
+     * @param height The height of the single line text label.
      */
     public void setSize(int x, int y, int width, int height) {
         singleLineTextLabel.setBounds(x, y, width, height);
